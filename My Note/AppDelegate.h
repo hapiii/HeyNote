97d17bd25b9a14ff19f3e75825e65ea192b92368
WQ <869932084@qq.com>
2016-07-18 11:21:22 +0800

    Initial Commit

---
+++ My Note/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  My Note
+//
+//  Created by 王强 on 16/7/18.
+//  Copyright © 2016年 王强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ My Note/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  My Note
+//
+//  Created by 王强 on 16/7/18.
+//  Copyright © 2016年 王强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
